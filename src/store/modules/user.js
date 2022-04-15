2bb883770f26f40c95fcfaa255f900929337837a
Gwj <1023128601@qq.com>
2022-04-16 00:17:22 +0800

    在vuex对token进行管理

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,12 +1,31 @@
+import { getToken, setToken, removeToken } from '@/utils/auth'
+import { login } from '@/api/user'
+
+const state = {
+  token: getToken() // 初始化vuex的时候就从缓存中读
+}
+const mutations = {
+  setToken(state, token) {
+    state.token = token // 将数据设置给vuex
+    // 同步给缓存
+    setToken(token)
+  },
+  removeToken(state) {
+    state.token = null
+    removeToken() // 同步到缓存
+  }
+}
+const actions = {
+  async login(context, data) {
+    const result = await login(data)
+    if (result.data.success) {
+      context.commit('setToken', result.data.data)
+    }
+  }
+}
 export default {
   namespaced: true,
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+  state,
+  mutations,
+  actions
 }
