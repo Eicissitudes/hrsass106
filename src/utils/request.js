5e0a66e751bff8ce7a2ac15e177a4487dd8fbd4c
Gwj <1023128601@qq.com>
2022-04-16 00:52:37 +0800

    处理axios的响应拦截器

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,25 @@
 import axios from 'axios'
-const service = axios.create()
+import { Message } from 'element-ui'
+const service = axios.create({
+  // 当执行npm run dev => 读取.env.developnment => api =>跨域代理
+  baseURL: process.env_VUE_APP_BASE_API, // npm run dev => /api
+  timeout: 5000
+})
 service.interceptors.request.use()
-service.interceptors.response.use()
+// 响应拦截器
+service.interceptors.response.use(response => {
+  // axios默认加了一层data
+  const { success, message, data } = response.data
+  // 要根据success的成功与否决定下面的操作
+  if (success) {
+    return data
+  } else {
+    //   业务已经错误了，不能进then,应该进catch
+    Message.error(message)
+    return Promise.reject(new Error(message))
+  }
+}, error => {
+  Message.error(error.message) // 提示错误信息
+  return Promise.reject(error) // 返回执行错误，将当前的执行链接跳出成功，直接进入catch
+})
 export default service
